f706dd665f01a0d763986ae8e085e99f7c01c6fa
Vipin Mahla <mahlavipin@gmail.com>
2024-08-14 20:26:24 +0530

    Added full functionality

--- composeApp/src/androidMain/kotlin/org/example/project/App.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/App.kt
@@ -1,36 +1,102 @@
 package org.example.project
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import org.koin.androidx.compose.koinViewModel
+import com.jetbrains.spacetutorial.entity.RocketLaunch
+import com.jetbrains.spacetutorial.RocketLaunchViewModel
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 
-import kmpnetworkinganddatastorage.composeapp.generated.resources.Res
-import kmpnetworkinganddatastorage.composeapp.generated.resources.compose_multiplatform
-
+@OptIn(
+    ExperimentalMaterial3Api::class
+)
 @Composable
-@Preview
 fun App() {
+    val viewModel = koinViewModel<RocketLaunchViewModel>()
+    val state by remember { viewModel.state }
+    val pullToRefreshState = rememberPullToRefreshState()
+    if (pullToRefreshState.isRefreshing) {
+        viewModel.loadLaunches()
+        pullToRefreshState.endRefresh()
+    }
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Scaffold(
+            topBar = {
+                TopAppBar(title = {
+                    Text(
+                        "SpaceX Launches",
+                        style = MaterialTheme.typography.headlineLarge
+                    )
+                })
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        ) { padding ->
+            Box(
+                modifier = Modifier
+                    .nestedScroll(pullToRefreshState.nestedScrollConnection)
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                if (state.isLoading) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text("Loading...", style = MaterialTheme.typography.bodyLarge)
+                    }
+                } else {
+                    LazyColumn {
+                        items(state.launches) { launch: RocketLaunch ->
+                            Column(modifier = Modifier.padding(all = 16.dp)) {
+                                Text(
+                                    text = "${launch.missionName} - ${launch.launchYear}",
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                                Spacer(Modifier.height(8.dp))
+                                Text(
+                                    text = if (launch.launchSuccess == true) "Successful" else "Unsuccessful",
+                                    color = if (launch.launchSuccess == true) Color(0xff4BB543) else Color(0xffFC100D)
+                                )
+                                Spacer(Modifier.height(8.dp))
+                                val details = launch.details
+                                if (details?.isNotBlank() == true) {
+                                    Text(
+                                        text = details
+                                    )
+                                }
+                            }
+                            HorizontalDivider()
+                        }
+                    }
                 }
+
+                PullToRefreshContainer(
+                    state = pullToRefreshState,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
             }
         }
     }
